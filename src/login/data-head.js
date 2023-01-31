ee40692f9f573b382c705d46f283245c9340afd4
dhananjaydavhale12 <54076005+dhananjaydavhale12@users.noreply.github.com>
2023-01-31 22:31:24 +0530

    Add files via upload

---
+++ src/login/data-head.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const dataHeder =[{
+  title:"home",
+  icon:<icon/>,
+  link:"/home"
+
+}
+]
+export default dataHeder;
---
+++ src/login/header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../App.css";
+
+
+const header =() =>{
+    return (
+      <div className="Header-bar">
+        <div className="hData">
+        <i className="ri-menu-line"></i>
+        </div>
+        <div className="title">The Brve </div>
+      </div>
+
+    );
+}
+
+export default header;
