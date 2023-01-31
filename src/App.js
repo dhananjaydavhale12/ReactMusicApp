784288b69cadeb3409ed41f6d02b4864629108f1
dhananjaydavhale12 <54076005+dhananjaydavhale12@users.noreply.github.com>
2023-01-31 22:29:18 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import {useEffect} from "react";
+import axios from "axios"
+
+
+
+function App() {
+
+  return (
+    <div className='container'>
+     <h1>Hello X how are you</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Favourites from "./pages/Favourites";
+import Playlist from "./pages/Playlist";
+import NoPage from "./pages/NoPage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="search" element={<Search />} />
+          <Route path="favourites" element={<Favourites />} />
+          <Route path="playlist" element={<Playlist />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
